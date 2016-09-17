d408712b4b39ccd6c8b8861647785bb6799949d0
Michael Sharifi <michael.a.sharifi@gmail.com>
2016-09-16 18:03:34 -0700

    mods to gen_x0 solver 9

--- gen_x0.cpp
+++ gen_x0.cpp
@@ -28,7 +28,7 @@ vector<double> gen_x0(double coh_in, double b_min, void *vf1_in, void *vf2_in, v
 
 	int N_controlh;
 	double h_step0 = 0.2;
-	double h_step_mult = 0.5; //0.25;
+	double h_step_mult = 0.25; //0.25;
 	double h_step = h_step0;
 
 	vector<double> x0 = x0_in;    
