977cd18ab16c6db9c8726260a7a876d0c1469ba7
Michael Sharifi <michael.a.sharifi@gmail.com>
2016-10-05 18:31:26 -0700

    adding income disaster12

--- vfn.cpp
+++ vfn.cpp
@@ -318,7 +318,9 @@ void vfn::set_terminal(double phr_in) {
 				w_adj = w_grid[i_w] - (*snodes1).ten_w[i_t]*(*snodes1).p_gridt[t_num][i_ph3]; 
 	
 				//coh_perm = (rb - 1.0)*w_adj  + 1.0*(rb - 1.0)*10.0*y_atax*y_replace*(*snodes1).yi_gridt[T_max][i_yi];
-				coh_perm = (rb - 1.0)*w_adj + y_replace*(*snodes1).yi_gridt[T_max][i_yi];
+				
+				//coh_perm = (rb - 1.0)*w_adj + y_replace*(*snodes1).yi_gridt[T_max][i_yi];
+				coh_perm = 1.0/16.0*w_adj + y_replace*(*snodes1).yi_gridt[T_max][i_yi];
 
 				if (i_t == 0) {
 					coh_perm = coh_perm - 1.0*(*snodes1).rent_adj * (*snodes1).rent_gridt[T_max][i_rent];
